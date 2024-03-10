9a0bb80bd12b6a55014c0dc17bfc90c2f183bed5
jarondlk <jaronchai78@gmail.com>
2024-03-10 13:50:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project155/Activities/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.project155.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.project155.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userEdt, passEdt;
+    private Button loginBtn;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initView();
+    }
+
+    private void initView() {
+        userEdt = findViewById(R.id.editTextText);
+        passEdt = findViewById(R.id.editTextPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            String email = userEdt.getText().toString();
+            String password = passEdt.getText().toString();
+            signIn(email, password);
+        });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish(); // Close login activity to prevent returning to it
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/project155/Domain/Genres.java
@@ -0,0 +1,19 @@
+
+package com.example.project155.Domain;
+
+
+import java.util.List;
+
+
+public class Genres {
+
+  private List<GenresItem> genres;
+
+    public List<GenresItem> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenresItem> genres) {
+        this.genres = genres;
+    }
+}
